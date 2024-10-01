b9343e1d0361251421e61ba3896971a4b4ed1d45
AhmedSherif237 <sh0803970@gmail.com>
2024-10-01 13:47:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -64,3 +64,41 @@ document.getElementById("toggleAchievements").addEventListener("click", function
     }
   });
 });
+
+// إظهار الزر عند التمرير لأسفل
+window.onscroll = function() {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  if (window.scrollY > 300) {
+    scrollToTopBtn.classList.add("show");  // إظهار الزر
+  } else {
+    scrollToTopBtn.classList.remove("show");  // إخفاء الزر
+  }
+};
+
+// التمرير إلى الأعلى عند النقر على الزر
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"  // تمرير سلس إلى الأعلى
+  });
+});
+
+
+window.onload = function() {
+  // تعطيل التمرير أثناء عرض شاشة التحميل
+  document.body.style.overflow = 'hidden';
+
+  setTimeout(function() {
+    // إخفاء شاشة التحميل مع تأثير التلاشي
+    const loadingScreen = document.getElementById("loading");
+    loadingScreen.style.animation = "fadeOut 0.5s forwards"; // إضافة تأثير التلاشي عند الإخفاء
+
+    setTimeout(function() {
+      loadingScreen.style.display = "none"; // إخفاء العنصر بعد انتهاء تأثير التلاشي
+      document.body.style.overflow = 'auto'; // إعادة تمكين التمرير
+    }, 500); // انتظار 500 مللي ثانية لتتناسب مع مدة تأثير التلاشي
+  }, 3000); // إخفاء الشاشة بعد 3 ثوانٍ
+};
+
+
